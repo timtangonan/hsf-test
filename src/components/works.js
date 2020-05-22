11ca7b7be9ee83ae978f065f778a6ba9ad4c2d69
Tim Tangonan <trstangonan@gmail.com>
2020-05-22 23:23:49 +0800

    works section started

--- src/components/works.js
+++ src/components/works.js
@@ -7,36 +7,40 @@ import aspirationImage from "../assets/2.png"
 import designImage from "../assets/3.png"
 import lustImage from "../assets/4.png"
 
+import "../styles/works.css"
+
 const Works = () => {
     return (
-        <section>
-            <div>
-                <p>check out</p>
-                <h2>our worx</h2>
+        <section className="works-main">
+            <div className="container">
+                <div className="works-intro">
+                    <p>check out</p>
+                    <h2>our worx</h2>
+                </div>
+                <div className="work-list">
+                    <div className="work-category">
+                        <img src={desireImage} alt="desire"/>
+                        <h3>desire</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros ligula, lacinia quis euismod in, dignissim at neque. Aliquam quis consectetur magna.</p>
+                    </div>
+                    <div className="work-category">
+                        <img src={aspirationImage} alt="aspiration"/>
+                        <h3>aspiration</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros ligula, lacinia quis euismod in, dignissim at neque. Aliquam quis consectetur magna.</p>
+                    </div>
+                    <div className="work-category">
+                        <img src={designImage} alt="design"/>
+                        <h3>design</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros ligula, lacinia quis euismod in, dignissim at neque. Aliquam quis consectetur magna.</p>
+                    </div>
+                    <div className="work-category">
+                        <img src={lustImage} alt="lust"/>
+                        <h3>Lust</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros ligula, lacinia quis euismod in, dignissim at neque. Aliquam quis consectetur magna.</p>
+                    </div>
+                </div>
+                <CTAButtons />
             </div>
-            <div>
-                <div className="work-category">
-                    <img src={desireImage} alt="desire"/>
-                    <h3>desire</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros ligula, lacinia quis euismod in, dignissim at neque. Aliquam quis consectetur magna.</p>
-                </div>
-                <div className="work-category">
-                    <img src={aspirationImage} alt="aspiration"/>
-                    <h3>aspiration</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros ligula, lacinia quis euismod in, dignissim at neque. Aliquam quis consectetur magna.</p>
-                </div>
-                <div className="work-category">
-                    <img src={designImage} alt="design"/>
-                    <h3>design</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros ligula, lacinia quis euismod in, dignissim at neque. Aliquam quis consectetur magna.</p>
-                </div>
-                <div className="work-category">
-                    <img src={lustImage} alt="lust"/>
-                    <h3>Lust</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros ligula, lacinia quis euismod in, dignissim at neque. Aliquam quis consectetur magna.</p>
-                </div>
-            </div>
-            <CTAButtons />
         </section>
     )
 }
